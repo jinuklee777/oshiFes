613899b3ff0099a8123f16a91d967b00e426edde
jinuklee777 <jinuklee777@soongsil.ac.kr>
2026-04-02 14:07:17 +0900

    Initial Commit

---
+++ src/test/java/com/oshifes/backend/OshiFesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshifes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshiFesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
